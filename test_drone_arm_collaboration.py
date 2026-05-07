891c37f0ac59d852edf6b4c72efb09e7075d41f8
Lin00niL-Ruolin <1120313692@qq.com>
2026-05-07 18:18:45 +0800

    托盘高度

--- test_drone_arm_collaboration.py
+++ test_drone_arm_collaboration.py
@@ -119,7 +119,7 @@ class DroneArmCollaborationTest:
             tray_id = self.client.load_object(
                 obj_name="tray",
                 model_path="Asset/Scene/Object/URDF_models/clear_box/model.urdf",
-                object_position=[0.8, 0.2, 1.3],
+                object_position=[0.8, 0.2, 1.4],
                 object_orientation=[0, 0, 0, 1],
                 scale=2.2,
                 fixed_base=True
