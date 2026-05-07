680810f6315d6319559ac0a957a536fdb40cdd33
Lin00niL-Ruolin <1120313692@qq.com>
2026-05-07 21:16:43 +0800

    放杯子高度

--- test_drone_arm_collaboration.py
+++ test_drone_arm_collaboration.py
@@ -255,7 +255,7 @@ class DroneArmCollaborationTest:
         # 步骤 1.4: 导航到桌子上方
         print("\n   [1.4] Lucy 导航到桌子上方...")
         # 桌子高度约0.8米，放置高度约0.9米
-        place_pos = [table_pos[0], table_pos[1], table_pos[2]+0.1]
+        place_pos = [table_pos[0], table_pos[1], table_pos[2]+0.3]
         hover_pos = [place_pos[0]-0.2, place_pos[1]-0.2, place_pos[2] + 3.0]
         hover_pos2 = [place_pos[0]-0.2, place_pos[1]-0.2, place_pos[2]+0.5]
 
